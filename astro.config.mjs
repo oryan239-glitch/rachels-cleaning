ae5324a0201524249ad97269ecf19340c027bc14
Oryan Atkinson <Oryan239@gmail.com>
2026-09-21 23:54:12 +0000

    Set production site URL for Cloudflare domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 export default defineConfig({
-  site: 'https://rachelscleaning.net',
+  site: 'https://rachelscleaningswfl.com',
   output: 'static',
   compressHTML: true,
 });
